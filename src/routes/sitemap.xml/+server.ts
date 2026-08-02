a6f50eadec5fe29a3971043145aabf1495b0c130
Frankenstein401 <abdulganihadiansyah@gmail.com>
2026-08-02 19:41:59 +0700

    Optimize sitemap.xml to strict XML format

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -15,23 +15,19 @@ export const GET: RequestHandler = async () => {
     "/contact"
   ];
 
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  ${pages
+  const urls = pages
     .map(
-      (page) => `
-  <url>
-    <loc>${SITE_URL}${page}</loc>
-    <changefreq>weekly</changefreq>
-    <priority>${page === "" || page === "/about" ? "1.0" : "0.8"}</priority>
-  </url>`
+      (page) =>
+        `<url><loc>${SITE_URL}${page}</loc><changefreq>weekly</changefreq><priority>${page === "" || page === "/about" ? "1.0" : "0.8"}</priority></url>`
     )
-    .join("")}
-</urlset>`;
+    .join("");
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${urls}</urlset>`;
 
   return new Response(sitemap, {
     headers: {
-      "Content-Type": "application/xml"
+      "Content-Type": "application/xml; charset=utf-8",
+      "Cache-Control": "max-age=0, s-maxage=3600"
     }
   });
 };
